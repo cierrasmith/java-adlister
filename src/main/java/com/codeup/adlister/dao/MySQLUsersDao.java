db09c9a3f959663771ca91a354e5d0d1646dd517
Cierra Smith <cierra.r.smith3@gmail.com>
2021-09-13 11:03:42 -0500

    updated user-input-exercise

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,12 +1,10 @@
 package com.codeup.adlister.dao;
 
+import com.codeup.adlister.Config;
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
 
 import java.sql.*;
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class MySQLUsersDao implements Users{
 
@@ -27,11 +25,50 @@ public class MySQLUsersDao implements Users{
 
     @Override
     public User findByUsername(String username) {
+
+        String query = "select * from users where username = ? limit 1";
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, username);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()) {
+                return new User(
+                        rs.getLong("id"),
+                        rs.getString("username"),
+                        rs.getString("email"),
+                        rs.getString("password")
+                );
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     @Override
     public Long insert(User user) {
-        return null;
+        String query = "insert into users(username, email, password) values(?, ?, ?)";
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new user.", e);
+        }
     }
 }
